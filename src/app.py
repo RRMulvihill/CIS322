0588199d4f0dc59d8b1dc119b00edb53bd1a62f7
WellDressedMan <Friskog55@GMail.com>
2017-02-06 02:11:48 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,20 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from config import dbname, dbhost, dbport
 import psycopg2
 import requests
 
 app = Flask(__name__)
 
-session = requests.session()
 
-conn = psycopg2.connect("dbname=lost host='\tmp/'")
+conn = psycopg2.connect("dbname=lost host='/tmp/'")
 cur = conn.cursor()
-cur.execute("SELECT * FROM assets;")
-res = cur.fetchall()
-processed_data = []
-for r in res:
-    processed_data.append( dict(zip(('column_name1', 'column_name2'), r)) )
-    session['processed_data_session_name'] = processed_data 
+
 @app.route('/')
 def login():
     return render_template('login.html',dbname=dbname,dbhost=dbhost,dbport=dbport)
@@ -25,13 +19,19 @@ def report():
 
 @app.route('/facility')
 def facility():
+    cur.execute("SELECT * FROM assets;")
+    res = cur.fetchall()
+    processed_data = []
+    for r in res:
+        processed_data.append( dict(zip(('column_name1', 'column_name2'), r)) )
+        session['processed_data_session_name'] = processed_data 
     if request.method=='GET' and 'fcName' in request.args:
         return render_template('facility.html',data=request.args.get('fcName'))
     return render_template('facility.html',data=request.args.get('fcName'))
 
 @app.route('/transit')
 def transit():
-        return render_template('transit.html',data=request.args.get('tranName'))
+    return render_template('transit.html',data=request.args.get('tranName'))
                                
 @app.route('/logout')
 def logout():
