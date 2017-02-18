ab23e27911440e970470aa7ab7c9decb48334e4e
WellDressedMan <Friskog55@GMail.com>
2017-02-18 14:06:34 -0800

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,4 +2,8 @@ CREATE TABLE user (
 	user varchar(16),
 	password varchar(16)
 );
---I chose not to give a primary key at this time as it is not needed, sessons will use the username not an integer id. I am creating code that fufills the requsted output, so my code is streamlined and not burdened with currenlty unecessary code. The varchar feilds are corresponding to the assignemnt instructions "no longer than 16 characters"
+--I chose not to give a primary key at this time as it is not needed, 
+--sessons will use the username not an integer id.
+--I am creating code that fufills the requsted output,
+--so my code is streamlined and not burdened with currenlty unecessary code.
+--The varchar feilds are corresponding to the assignemnt instructions "no longer than 16 characters"
