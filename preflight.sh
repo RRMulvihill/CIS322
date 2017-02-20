ce5094e73303618db9db6e8ce5254f60526e59c3
WellDressedMan <Friskog55@GMail.com>
2017-02-19 19:03:50 -0800

    Update preflight.sh

--- preflight.sh
+++ preflight.sh
@@ -1,1 +1,13 @@
-#echo "hello world"
+#! /usr/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./preflight.sh <dbname>"
+    exit;
+fi
+
+# Database prep
+cd sql
+psql -f create_tables.sql $1 
+cd ..
+
+# Install the wsgi files
+cp -R src/* $HOME/wsgi
