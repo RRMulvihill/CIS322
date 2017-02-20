f2753e55049843206a680a081c15d792b18c66f4
WellDressedMan <Friskog55@GMail.com>
2017-02-19 19:03:56 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -8,5 +8,19 @@ app = Flask(__name__)
 
 @app.route('/')
 
+@app.route('/create_user' methods=['GET', 'POST'])
+def create_user():
+    if request.method =='POST':
+        return render_template('create_user.html')
+    if request.method == 'POST':
+        username = request.form['uname']
+        password = request.form['pass']
+        session['user'] = username
+        conn = psycopg2.connect(dbname=dbname,host=dbhost,port=dbport)
+        cur  = conn.cursor()
+        cur.execute('SELECT username FROM users WHERE username = %s', (username))
+        res = cur.fetchall()
+        return render_template('create_user.html')
+
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=8080)
