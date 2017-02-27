b0c0de3397d327729c480751a62cf730a0923cc5
WellDressedMan <Friskog55@GMail.com>
2017-02-27 00:31:57 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -92,7 +92,7 @@ def add_asset():
 @app.route('/dispose_asset', methods=['GET', 'POST'])
 def dispose_asset():
         conn = psycopg2.connect(dbname=dbname, host=dbhost,port=dbport)
-		cur = conn.cursor()
+	cur = conn.cursor()
         cur.excecute("SELECT status_pk FROM asset_at where status = 'disposed';")
         status_fk = cur.fetchone()[0]
 		cur.execute("SELECT * FROM assets WHERE status_fk = '%s';"%(status_fk))
