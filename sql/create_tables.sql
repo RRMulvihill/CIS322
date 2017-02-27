0cb4012774a542d0a7b67839b4c29fa15d041fc5
WellDressedMan <Friskog55@GMail.com>
2017-02-26 16:43:30 -0800

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
 	username varchar(16),
 	password varchar(16),
-	role_fk integer
+	role_fk integer REFERENCES roles(role_pk)
 );
 --I chose not to give a primary key at this time as it is not needed, 
 --sessons will use the username not an integer id.
@@ -9,14 +9,14 @@ CREATE TABLE users (
 --so my code is streamlined and not burdened with currenlty unecessary code.
 --The varchar feilds are corresponding to the assignemnt instructions "no longer than 16 characters"
 Create TABLE roles (
-	role_pk integer,
-	role varchar(25)
+	role_pk serial primary key,
+	role varchar(18)
 );
 CREATE TABLE assets (
 	asset_tag varchar(16),
 	description text
-	facility_fk integer,
-	status integer
+	facility_fk integer REFERENCES facilities(fac_pk),
+	status_fk integer REFERENCES asset_at(status_pk)
 );
 CREATE TABLE asset_at (
 	status_pk serial primary key,
@@ -27,3 +27,7 @@ CREATE TABLE facilities (
 	fac_name varchar(32),
 	fac_code varchar(6)
 );
+
+
+INSERT INTO roles (role) VALUES ('Logistics Officer');
+INSERT INTO roles (role) VALUES ('Facilities Officer')
