e696b3c7c7bab864acf0e663b61e1be45cfeccad
WellDressedMan <Friskog55@GMail.com>
2017-03-12 01:36:23 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -77,11 +77,9 @@ def add_asset():
 		if cur.fetchone() is not None:
 			return render_template('entry_exists.html')
 		else:
-			cur.execute("SELECT fac_pk FROM facilities where fac_code = '%s'"%(fac_code))
-			fac_fk = cur.fetchone()
 			cur.execute("SELECT status_pk FROM asset_at where status = 'at_facility';")
 			fac_fk = cur.fetchone()[0]
-			cur.execute("INSERT INTO assets(asset_tag,description,fac_fk,status_fk) VALUES ('%s', '%s','%s'));"%(asset_tag,description,fac_fk,'at_facility'))
+			cur.execute("INSERT INTO assets(asset_tag,description,fac_fk,status_fk) VALUES ('%s', '%s','%s',%s));"%(asset_tag,description,fac_fk,'at_facility'))
 			conn.commit()
 			return render_template('entry_created.html')  
 @app.route('/dispose_asset', methods=['GET', 'POST'])
