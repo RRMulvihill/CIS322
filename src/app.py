169b3eec39ad07c0f65415a37034209c8ac5f5d3
WellDressedMan <Friskog55@GMail.com>
2017-03-12 14:19:40 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -132,7 +132,7 @@ def dashboard():
 	if session['role'] == "Logistics Officer":
 		columns=[('Transit ID'),('Asset Tag'),('Source Facility'),('Destination Facilility'),('Approval Date')]
 		sql = "SELECT transits.req_fk, assets.asset_tag,facilities.fac_name,facilities.fac_name,requests.approved_dt FROM transits AS t INNER JOIN assets AS a ON a.asset_pk = t.asset_fk INNER JOIN facilities AS f ON (f.fac_pk = t.source_fk) or (f.fac_pk = t.destination_fk) INNER JOIN requests AS r ON r.req_pk = t.req_fk"
-		ltasks = query(sql,(,))
+		ltasks = query(sql,)
 		return render_template('dashboard.html',columns=columns,ltasks = ltasks,ftasks=blank)
 	else:
 		headers=[('Transit ID'), ('Asset Tag'), ('Source Facilitiy'), ('Destination Facility'), ('Request Date')]
