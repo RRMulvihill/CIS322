c6c5ffab7f0d6d3d9062e1b0a2a099cf5db5e7ae
WellDressedMan <Friskog55@GMail.com>
2017-03-12 18:02:22 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -143,7 +143,7 @@ def dashboard():
 		headers=[('Transit ID'), ('Asset Tag'), ('Source Facilitiy'), ('Destination Facility'), ('Request Date')]
 		sql = "SELECT transits.req_fk, assets.asset_tag,facilities.fac_name,facilities.fac_name,requests.approved_dt FROM transits AS t INNER JOIN assets AS a ON a.asset_pk = t.asset_fk INNER JOIN facilities AS f ON (f.fac_pk = t.source_fk) or (f.fac_pk = t.destination_fk) INNER JOIN requests AS r ON r.req_pk = t.req_fk WHERE r.approved='FALSE';"
 		fres = query(sql,())
-		return render_template('dashboard.html'tableheader=headers, ltasks = blank,ftasks=fres)
+		return render_template('dashboard.html',tableheader=headers, ltasks = blank,ftasks=fres)
 @app.route('/transfer_req', methods=['GET','POST'])
 def transfer_req():
 	if session['role'] != 'Logistics Officer':
