59e23f9f4031168f9386397e2b821469666c5f51
WellDressedMan <Friskog55@GMail.com>
2017-03-12 17:13:51 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, request, url_for, session
 from config import dbname, dbhost, dbport
 import json
 import psycopg2
