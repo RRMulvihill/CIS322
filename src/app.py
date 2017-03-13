e549d9fc0455a3c845ff851debefe0c461abc9e3
WellDressedMan <Friskog55@GMail.com>
2017-03-12 17:04:15 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -140,7 +140,7 @@ def dashboard():
 		ltasks = query(sql,())
 		return render_template('dashboard.html',columns=columns,ltasks = ltasks,ftasks=blank)
 	else:
-		headers=[('Transit ID'), ('Asset Tag'), ('Source Facilitiy'), ('Destination Facility'), ('Request Date')]
+		columns=[('Transit ID'), ('Asset Tag'), ('Source Facilitiy'), ('Destination Facility'), ('Request Date')]
 		sql = "SELECT transits.req_fk, assets.asset_tag,facilities.fac_name,facilities.fac_name,requests.approved_dt FROM transits AS t INNER JOIN assets AS a ON a.asset_pk = t.asset_fk INNER JOIN facilities AS f ON (f.fac_pk = t.source_fk) or (f.fac_pk = t.destination_fk) INNER JOIN requests AS r ON r.req_pk = t.req_fk WHERE r.approved='FALSE';"
 		ftasks = query(sql,())
 		return render_template('dashboard.html',columns=columns,ltasks = blank,ftasks=ftasks)
