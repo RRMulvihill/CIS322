9326045a239c046870e126eb911997c3c08d28c5
WellDressedMan <Friskog55@GMail.com>
2017-03-14 13:03:01 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -256,7 +256,7 @@ def update_transit():
 		return redirect('dashboard')
 	if request.method=='GET':
 		session['req_fk'] = int(request.args['id'])
-		sql = "SELECT r.req_pk,a.asset_tag,s.fac_name,d.fac_name,r.submit_dt, FROM requests AS r INNER JOIN assets AS a ON r.asset_fk = a.asset_pk INNER JOIN facilities AS s ON s.fac_pk = r.source_fk INNER JOIN facilities AS d ON d.fac_pk = r.destination_fk WHERE r.req_pk = %s;"
+		sql = "SELECT r.req_pk,a.asset_tag,s.fac_name,d.fac_name,r.submit_dt FROM requests AS r INNER JOIN assets AS a ON r.asset_fk = a.asset_pk INNER JOIN facilities AS s ON s.fac_pk = r.source_fk INNER JOIN facilities AS d ON d.fac_pk = r.destination_fk WHERE r.req_pk = %s;"
 		req_data = query(sql,(session['req_fk'],))
 		res=dict()
 		res['id']=req_data[0][0]
