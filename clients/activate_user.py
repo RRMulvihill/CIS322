ac5f2a958dd71adfdd5a8e8286085e72e4a1872a
WellDressedMan <Friskog55@GMail.com>
2017-03-19 18:12:22 -0700

    Update activate_user.py

--- clients/activate_user.py
+++ clients/activate_user.py
@@ -5,37 +5,38 @@ import json
 from urllib.request import Request, urlopen
 from urllib.parse   import urlencode
 
-def main():
+main()
     # Check the CLI arguments
-    if len(sys.argv)<5 :
+    if len(sys.argv) !=5 :
         print("Usage: python3 %s <url> <username> <password> <role>"%sys.argv[0])
-        return
+        sys.exit()
     
     # Prep the arguments blob
     args = dict()
     args['username']  = sys.argv[2]
     args['password'] = sys.argv[3]
-    args['role'] = sys.argv[4]
+    
+    #get role
+    if (sys.argv[4] != 'logofc' and sys.argv[4] != 'facofc'):
+        print("role must be either 'logofc' or 'facofc'")
+        sys.exit()
+    if sys.argv[4] == 'logofc':
+        args['role'] = 1
+    if sys.argv[4] == 'facofc':
+        args['role'] = 2
+    
+    data = urlencode(args)
 
     # Print a message to let the user know what is being tried
     print("Activating user: %s pass: %s role: %s"%args['username'],args['password'],args['role'])
-
-    # Setup the data to send
-    sargs = dict()
-    sargs['arguments']=json.dumps(args)
-    data = urlencode(args)
-    print("sending:\n%s"%data)
     
     # Make the resquest
-    path = sys.argv[1] + 'create_user'
+    path = sys.argv[1] + 'activate_user'
     req = Request(path,data.encode('ascii'),method='POST')
     res = urlopen(req)
     
-    # Parse the response
-    resp = json.loads(res.read().decode('ascii'))
-    
     # Print the result code
-    print("Call to LOST returned: %s"%resp)
+    print(res.read().decode('ascii'))
     
 
 if __name__=='__main__':
