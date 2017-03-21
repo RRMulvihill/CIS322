69e6c716fc073b86303536e07312e2a0f25c56a3
WellDressedMan <Friskog55@GMail.com>
2017-03-21 11:19:47 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -130,7 +130,7 @@ def add_asset():
 			sql = "SELECT fac_pk FROM facilities where fac_code = %s;"
 			fac_fk = query(sql,(fac_code,))
 			print(fac_fk[0][0])
-			sql = "INSERT INTO assets (asset_tag,date,description,fac_fk,disposed) VALUES (%s,%s,%s,%s,False);"
+			sql = "INSERT INTO assets (asset_tag,date,description,fac_fk,disposed) VALUES ('%s',%s,%s,%s,False);"
 			query(sql,(asset_tag,date, description,fac_fk[0][0]))
 			session['msg'] = 'asset created!'
 			return redirect('dashboard')  
